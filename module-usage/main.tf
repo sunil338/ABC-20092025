9c4f5958f408b2ab3a0ff017d41c05d616776a15
Sunil <sunilmuradi007@gmail.com>
2025-09-20 09:34:38 +0530

    Update main.tf

--- module-usage/main.tf
+++ module-usage/main.tf
@@ -1,15 +1,11 @@
-module "modres" {
-  source = "https://github.com/sunil338/ABC-20092025.git"
+module "abc" {
+  source = "git::https://github.com/sunil338/ABC-20092025.git//ABC?ref=main"
+
+  rg_name         = var.rg_name
+  location        = var.location
+  vnet1_cidr      = var.vnet1_cidr
+  vnet2_cidr      = var.vnet2_cidr
+  username        = var.username
+  public_key_path = var.public_key_path
 }
 
-output "vm1_public_ip" {
-  value = module.modres.vm1_public_ip
-}
-
-output "vm1_private_ip" {
-  value = module.modres.vm1_private_ip
-}
-
-output "vm2_private_ip" {
-  value = module.modres.vm2_private_ip
-}

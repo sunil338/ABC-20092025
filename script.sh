a7ba00d33ecb1cc55fcad3a89c336b16b6d74baa
Sunil <sunilmuradi007@gmail.com>
2025-09-20 12:03:10 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Variables
+VM1_IP="10.5.1.4"
+VM2_IP="10.15.1.4"
+USERNAME="azureuser"
+SSH_KEY_PATH="/home/azureuser/.ssh/id_rsa"
+
+echo "Testing connectivity from VM1 to VM2..."
+
+# Ping VM2 from VM1
+echo "Pinging VM2 from VM1..."
+ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no $USERNAME@$VM1_IP "ping -c 4 $VM2_IP"
+
+# Test SSH from VM1 to VM2
+echo "Testing SSH connectivity from VM1 to VM2..."
+ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no $USERNAME@$VM1_IP "ssh -i $SSH_KEY_PATH -o StrictHostKeyChecking=no $USERNAME@$VM2_IP 'echo SSH to VM2 successful!'"
